9f9016054b2563a540cfa6affa56882e5e6d0c55
Laura <la-u-r4.venus@hotmail.com>
2020-07-12 07:32:39 -0600

    primer commit

---
+++ js/main.js
@@ -0,0 +1,234 @@
+var typed1 = new Typed('#typed1 ', {
+    strings: [
+     '<i class="skills">JAVA</i>',
+     '<i class="skill">JAVA</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+
+var typed2 = new Typed('#typed2 ',  {
+    strings: [
+     '<i class="skills">JAVASCRIPT</i>',
+     '<i class="skill">JAVASCRIPT</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed3 = new Typed('#typed3 ',  {
+    strings: [
+     '<i class="skills">C#</i>',
+     '<i class="skill">C#</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+
+var typed4 = new Typed('#typed4 ',  {
+    strings: [
+     '<i class="skills">LARAVEL</i>',
+     '<i class="skill">LARAVEL</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+
+var typed5 = new Typed('#typed5 ',  {
+    strings: [
+     '<i class="skills">PHALCON</i>',
+     '<i class="skill">PHALCON</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed6 = new Typed('#typed6 ',  {
+    strings: [
+     '<i class="skills">.NET</i>',
+     '<i class="skill">.NET</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed7 = new Typed('#typed7 ',  {
+    strings: [
+     '<i class="skills">CSS</i>',
+     '<i class="skill">CSS</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed8 = new Typed('#typed8',  {
+    strings: [
+     '<i class="skills">SCSS</i>',
+     '<i class="skill">SCSS</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed9 = new Typed('#typed9',  {
+    strings: [
+     '<i class="skills">SQL</i>',
+     '<i class="skill">SQL</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed10 = new Typed('#typed10',  {
+    strings: [
+     '<i class="skills">HTML</i>',
+     '<i class="skill">HTML</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed11 = new Typed('#typed11',  {
+    strings: [
+     '<i class="skills">SQLSERVER</i>',
+     '<i class="skill">SQLSERVER</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+var typed8 = new Typed('#typed12',  {
+    strings: [
+     '<i class="skills">PLSQL</i>',
+     '<i class="skill">PLSQL</i>'
+    ],
+    //stringsElement:
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor: true,
+    cursorChar:'|',
+    contentType: 'html',
+
+});
+
+
+
